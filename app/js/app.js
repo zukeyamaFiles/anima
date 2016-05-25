b002ad184be48bb8ba5055520133c83b16928156
zui <fugos78@gmail.com>
2016-05-26 01:51:33 +0900

    ファイル追加

--- app/js/app.js
+++ app/js/app.js
@@ -4,107 +4,77 @@ var _createClass = function () { function defineProperties(target, props) { for 
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var OriMath = function () {
-  function OriMath() {
-    _classCallCheck(this, OriMath);
-  }
-
-  _createClass(OriMath, [{
-    key: "sin",
-    value: function sin(deg) {
-      return deg * (180 / Math.PI);
-    }
-  }, {
-    key: "cos",
-    value: function cos(rad) {
-      return rad * (Math.PI / 180);
-    }
-  }, {
-    key: "atan",
-    value: function atan(y, x) {
-      return Math.atan2(y, x);
-    }
-  }]);
-
-  return OriMath;
-}();
-
 var typing = function () {
   function typing() {
-    var _this = this;
-
     _classCallCheck(this, typing);
 
-    $(".restart").on("click", function () {
-
-      _this.restart();
-    });
+    if (!this.UA("chrome")) alert("googleChromeを推奨します！");
+    this.Timearr = 0;
+    this.miss = 0;
+    this.num = 0;
+    this.sec = 8;
+    this.langCount = 0;
+    this.sound = this.MP();
+    this.win = $(window);
+    this.word = $(".word");
+    this.word_label = $(".word_label");
+    this.overlay = $("#overlay");
+    this.start_btn = $(".btn", this.overlay);
+    this.innerEl = $(".inner", this.overlay).find("p");
+    this.restart = $(".restart");
+    this.timeTx = $(".time");
+    this.evaluation = $(".evaluation");
+    this.missTxt = $(".miss");
+    this.timeScore = $(".timeScore");
+    this.stageinner = $(".stageinner");
+    this.addEvent();
   }
 
   _createClass(typing, [{
     key: "set",
-    value: function set() {
-      var _this2 = this;
-
-      this.Timearr = 0;
-      this.miss = 0;
-      this.num = 0;
-      $(".btn").click(function (e) {
-        e.stopPropagation();
-        _this2.Time("timerTxt", 2);
-        _this2.sound = _this2.MP();
-        _this2.PlayBGM(_this2.sound.BGM);
-        $("#overlay").addClass("fade");
-        _this2.addEvent();
-      });
-      this.word = $(".word");
-      this.label_ = ["css", "daiya", "ryby", "scala", "java", "c"];
+    value: function set(a, b) {
+      this.PlayBGM(this.sound.BGM);
+      this.label_ = b;
       this.label_ = this.shuffle(this.label_);
       this.label_length = this.label_.length;
-      this.wrap_ward(this.label_, this.num);
+      this.word_label.html(this.label_[this.num]);
+      this.wrap_ward(a, this.label_[this.num]);
+      var typeaction = this.typeAction.bind(this);
+      this.win.on("keydown", typeaction);
     }
   }, {
-    key: "restart",
-    value: function restart() {
-      this.num = 0;
-      this.PlayBGM(this.sound.BGM);
-      $("#overlay").addClass("fade");
-      this.Time("timerTxt", 2);
-      this.addEvent();
-      this.word = $(".word");
-      this.label_ = ["css", "daiya", "ryby", "scala", "java", "c"];
-      this.label_ = this.shuffle(this.label_);
-      this.label_length = this.label_.length;
-      this.wrap_ward(this.label_, this.num);
+    key: "restartAction",
+    value: function restartAction() {
+      this.set(this.json, this.jsonKeys);
     }
   }, {
     key: "action",
     value: function action() {
       if (this.label_length - 1 < this.num) {
+        this.stageinner.hide();
         this.GameClear();
         return false;
       }
-      this.wrap_ward(this.label_, this.num);
+      this.wrap_ward(this.json, this.label_[this.num]);
     }
   }, {
     key: "wrap_ward",
     value: function wrap_ward(el, num) {
+      var _this = this;
+
       var lang = "";
       this.target_lang = el[num].split("");
       this.lang_length = this.target_lang.length;
       this.target_lang.forEach(function (i, s) {
-        lang += i.replace(/(\S)/g, '<span class="textSplitLoad' + s + '">$&</span>');
+        lang += _this.replaceWrap(i, s);
       });
       this.word.html(lang);
+      this.word_label.html(num);
     }
   }, {
-    key: "animationFrame",
-    value: function animationFrame() {
-      window.requestAnimationFrame = function () {
-        return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (callback, element) {
-          window.setTimeout(callback, 1000 / 60);
-        };
-      }();
+    key: "replaceWrap",
+    value: function replaceWrap(el, num) {
+      return el.replace(/(\S)/g, '<span class="textSplitLoad' + num + '">$&</span>');
     }
   }, {
     key: "shuffle",
@@ -123,37 +93,81 @@ var typing = function () {
   }, {
     key: "addEvent",
     value: function addEvent() {
+      var _this2 = this;
+
+      this.restart.on("click", function (e) {
+        _this2.langCount = 0;
+        _this2.num = 0;
+        _this2.Timearr = 0;
+        _this2.miss = 0;
+        _this2.stageinner.show();
+        _this2.restartAction();
+        _this2.startAction(e, _this2.restart);
+        _this2.overlay.on("webkitTransitionEnd transitionend", function (e) {
+          _this2.innerEl.not(".restart").each(function () {
+            $(this).text("");
+          });
+
+          _this2.overlay.off("webkitTransitionEnd transitionend");
+        });
+      });
+      this.start_btn.on("click", function (e) {
+        _this2.startAction(e, _this2.start_btn);
+      });
+
+      this.overlay.on("webkitTransitionEnd transitionend", function (e) {
+        _this2.start_btn.hide();
+        _this2.overlay.off("webkitTransitionEnd transitionend");
+      });
+    }
+  }, {
+    key: "typeAction",
+    value: function typeAction(evt) {
+      var code = this.char(this.langCount);
+      if (code == evt.keyCode) {
+        this.PlayBtnBGM(this.sound.btn);
+        this.word.find("span").eq(this.langCount).addClass("addColor");
+        this.langCount++;
+      } else {
+        this.PlayBtnBGM(this.sound.can);
+        this.miss++;
+      }
+      if (this.lang_length - 1 < this.langCount) {
+        this.word.html("");
+        this.num += 1;
+        this.langCount = 0;
+        this.Timearr += this.overTime;
+        this.overTime = this.sec;
+        this.action();
+      }
+    }
+  }, {
+    key: "char",
+    value: function char(ca) {
+      return this.target_lang[ca].toUpperCase().charCodeAt(0);
+    }
+  }, {
+    key: "startAction",
+    value: function startAction(e, btn) {
       var _this3 = this;
 
-      var i = 0;
-      $(window).on("keydown", function (evt) {
-
-        var code = _this3.target_lang[i].toUpperCase().charCodeAt(0);
-        if (code == evt.keyCode) {
-          _this3.PlayBtnBGM(_this3.sound.btn);
-          $(".word").find("span").eq(i).css("color", "red");
-          i++;
-        } else {
-          _this3.PlayBtnBGM(_this3.sound.can);
-          _this3.miss++;
-        }
-        if (_this3.lang_length - 1 < i) {
-          $(".word").html("");
-          _this3.num += 1;
-          i = 0;
-          _this3.Timearr += _this3.overTime;
-          _this3.overTime = 5;
-          _this3.action();
-        }
-      });
+      e.stopPropagation();
+      this.score = Date.now();
+      this.PlayBtnBGM(this.MP().start);
+      btn.addClass("start");
+      setTimeout(function () {
+        _this3.overlay.addClass("fade");
+        _this3.Time("timerTxt", 2);
+      }, 3000);
     }
   }, {
     key: "MP",
     value: function MP() {
       var MP4 = {
-        BGM: new Audio("./audio/test.mp3"),
+        BGM: new Audio("./audio/BGM.mp3"),
         btn: "./audio/btn.mp3",
-        can: "./audio/can.mp3"
+        can: "./audio/can.mp3",
+        start: "./audio/start.mp3"
       };
       return MP4;
     }
@@ -176,7 +190,7 @@ var typing = function () {
 
       var timer, s, update, overTime;
 
-      this.overTime = 5;
+      this.overTime = this.sec;
       s = Date.now();
       this.Timertxt = document.getElementById(el);
       this.update = function (dobble) {
@@ -199,35 +213,47 @@ var typing = function () {
   }, {
     key: "GemeOverTime",
     value: function GemeOverTime() {
-      $("#overlay .btn").text("タイムオーバー");
+      this.start_btn.text("タイムオーバー");
       this.removeGame();
-      $(".restart").show();
-      $("#overlay").removeClass("fade");
+      this.restart.show();
+      $(".overtime").text("タイムオーバー").show();
+      this.overlay.find("p").removeClass("start");
+      this.overlay.removeClass("fade");
     }
   }, {
     key: "removeGame",
     value: function removeGame() {
       this.sound.BGM.pause();
       this.sound.BGM.currentTime = 0;
-      $(window).off("keydown");
+      this.win.off("keydown");
     }
   }, {
     key: "GameClear",
     value: function GameClear() {
       this.removeGame();
       clearInterval(this.Gametimer);
-      $("#overlay .btn").text("ステージクリア！");
-      $(".time").addClass("visible");
-      $("#overlay").removeClass("fade");
+      this.word_label.html("");
+      this.overlay.find("p").removeClass("start");
+      this.start_btn.text("ステージクリア！");
+      this.timeTx.addClass("visible");
+      this.overlay.removeClass("fade");
       var EvaluationTxt = this.Evaluation(this.Timearr);
-      $(".restart").text(EvaluationTxt + "<br>ミス" + this.miss + "回");
-      $(".restart").show();
+      this.evaluation.text(EvaluationTxt);
+      this.missTxt.text("タイプミス: " + this.miss + " 回");
+      this.timeScore.text("クリアータイム: " + this.timeCalculation(1) + " 秒");
+      this.restart.show();
+      this.missTxt.show();
+    }
+  }, {
+    key: "timeCalculation",
+    value: function timeCalculation(num) {
+      return ((Date.now() - this.score) / 1000).toFixed(num);
     }
   }, {
     key: "Evaluation",
     value: function Evaluation(val) {
-      var _val = Math.floor(val / this.label_.length);
-      var EvaluationTxt;
+      var _val = this.EvaluationNum(val, this.label_.length),
+          EvaluationTxt;
       switch (_val) {
         case 5:
           EvaluationTxt = "あなたはとても優秀ですね";
@@ -245,12 +271,75 @@ var typing = function () {
           EvaluationTxt = "ダメダメですね";
           break;
       }
-
       return EvaluationTxt;
+    }
+  }, {
+    key: "EvaluationNum",
+    value: function EvaluationNum(val, len) {
+      return Math.floor(val / len);
+    }
+  }, {
+    key: "getWord",
+    value: function getWord(pas) {
+      var _this5 = this;
+
+      var WORD = [];
+      var KEYS = [];
+      $.ajax({
+        url: pas,
+        dataType: "json",
+        cache: false,
+        success: function success(data, textStatus) {
+          _this5.json = data.item;
+          _this5.jsonKeys = Object.keys(data.item);
+          _this5.set(_this5.json, _this5.jsonKeys);
+        },
+        error: function error(xhr, textStatus, errorThrown) {
+          alert("読み込みに失敗しました");
+        }
+      });
+    }
+  }, {
+    key: "UA",
+    value: function UA(ua) {
+      var isUA = function () {
+        var ua = navigator.userAgent.toLowerCase(),
+            indexOfKey = function indexOfKey(key) {
+          return ua.indexOf(key) != -1 ? true : false;
+        };
+        var o = {};
+        o.ie = function () {
+          return indexOfKey("msie 8");
+        };
+        o.fx = function () {
+          return indexOfKey("firefox");
+        };
+        o.chrome = function () {
+          return indexOfKey("chrome");
+        };
+        o.opera = function () {
+          return indexOfKey("opera");
+        };
+        o.android = function () {
+          return indexOfKey("android");
+        };
+        o.ipad = function () {
+          return indexOfKey("ipad");
+        };
+        o.ipod = function () {
+          return indexOfKey("ipod");
+        };
+        o.iphone = function () {
+          return indexOfKey("iphone");
+        };
+        return o;
+      }();
+
+      return isUA[ua]();
     }
   }]);
 
   return typing;
 }();
 
-new typing().set();
+new typing().getWord("json/ikimono.json");
--- es6/app.js
+++ es6/app.js
@@ -1,97 +1,67 @@
-class OriMath {
-  constructor(){
-
-    
-  }
-  sin(deg){
-    return deg * (180/Math.PI);
-  }
-
-  cos(rad){
-    return rad * (Math.PI/180);    
-  }
-
-  atan(y,x){
-    return Math.atan2(y,x);
-  }
-}
-
-
 
 
 class typing {
+
   constructor(){
-    
-    $(".restart").on("click",() => {
-
-      this.restart();
-
-
-    });
-
-  }
-
-  set(){
+    if(!this.UA("chrome")) alert("googleChromeを推奨します！")
     this.Timearr = 0;
     this.miss = 0;
-  this.num = 0;
-  $(".btn").click((e) => {
-    e.stopPropagation();
-    this.Time("timerTxt",2);
+    this.num = 0;
+    this.sec = 8;
+    this.langCount = 0;
     this.sound = this.MP();
-    this.PlayBGM(this.sound.BGM);
-    $("#overlay").addClass("fade");
-    this.addEvent();
-  })
+    this.win = $(window);
     this.word = $(".word");
-    this.label_ = ["css","daiya","ryby","scala","java","c"];
-    this.label_ =  this.shuffle (this.label_);
-    this.label_length = this.label_.length;
-    this.wrap_ward(this.label_,this.num);
+    this.word_label = $(".word_label");
+    this.overlay = $("#overlay");
+    this.start_btn = $(".btn",this.overlay);
+    this.innerEl = $(".inner",this.overlay).find("p");
+    this.restart = $(".restart");
+    this.timeTx = $(".time");
+    this.evaluation = $(".evaluation");
+    this.missTxt = $(".miss");
+    this.timeScore = $(".timeScore");
+    this.stageinner = $(".stageinner");
+    this.addEvent();
   }
 
-  restart(){
-    this.num = 0;
+  set(a,b){
     this.PlayBGM(this.sound.BGM);
-    $("#overlay").addClass("fade");
-    this.Time("timerTxt",2);
-    this.addEvent();
-    this.word = $(".word");
-    this.label_ = ["css","daiya","ryby","scala","java","c"];
-    this.label_ =  this.shuffle (this.label_);
+    this.label_ = b;
+    this.label_ =  this.shuffle(this.label_);
     this.label_length = this.label_.length;
-    this.wrap_ward(this.label_,this.num);   
+    this.word_label.html(this.label_[this.num]);
+    this.wrap_ward(a,this.label_[this.num]);
+    var typeaction = this.typeAction.bind(this);
+    this.win.on("keydown",typeaction);
+  }
+
+  restartAction(){
+    this.set(this.json,this.jsonKeys);
   }
 
   action(){
     if(this.label_length -1 < this.num ){
+      this.stageinner.hide();
       this.GameClear();
       return false;
     }
-    this.wrap_ward(this.label_,this.num);
+    this.wrap_ward(this.json,this.label_[this.num]);
   }
 
   wrap_ward(el,num){
     var lang = "";
     this.target_lang = el[num].split("");
     this.lang_length = this.target_lang.length;
-    this.target_lang.forEach(function(i,s){
-      lang += i.replace(/(\S)/g, '<span class="textSplitLoad'+ s +'">$&</span>'); 
+    this.target_lang.forEach((i,s) => {
+      lang += this.replaceWrap(i,s); 
     });
     this.word.html(lang);
+    this.word_label.html(num);
   }
 
-  animationFrame(){
-     window.requestAnimationFrame = (function(){
-      return window.requestAnimationFrame   ||
-        window.webkitRequestAnimationFrame  ||
-        window.mozRequestAnimationFrame   ||
-        window.oRequestAnimationFrame   ||
-        window.msRequestAnimationFrame    ||
-        function(callback, element){
-          window.setTimeout(callback, 1000 / 60);
-        };
-    })();
+  replaceWrap(el,num){
+    return el.replace(/(\S)/g, '<span class="textSplitLoad'+ num +'">$&</span>');
   }
 
   shuffle (array){
@@ -106,34 +76,74 @@ class typing {
   }
 
   addEvent (){
-    var i = 0;
-    $(window).on("keydown",(evt) => {
+    this.restart.on("click",(e) => {
+      this.langCount = 0;
+      this.num = 0;
+      this.Timearr = 0;
+      this.miss = 0;
+      this.stageinner.show();
+      this.restartAction();
+      this.startAction(e,this.restart);
+      this.overlay.on("webkitTransitionEnd transitionend",(e) => {
+        this.innerEl.not(".restart").each(function(){
+          $(this).text("");
+        })
 
-      var code = this.target_lang[i].toUpperCase().charCodeAt(0);
+        this.overlay.off("webkitTransitionEnd transitionend");
+      });
+    });
+    this.start_btn.on("click",(e) => {
+      this.startAction(e,this.start_btn);
+    });
+
+    this.overlay.on("webkitTransitionEnd transitionend",(e) => {
+      this.start_btn.hide();
+      this.overlay.off("webkitTransitionEnd transitionend");
+    });
+
+  }
+
+  typeAction(evt){
+      var code = this.char(this.langCount);
       if(code == evt.keyCode) {
         this.PlayBtnBGM(this.sound.btn);
-        $(".word").find("span").eq(i).css("color","red");
-        i++;
+        this.word.find("span").eq(this.langCount).addClass("addColor");
+        this.langCount++;
       } else {
           this.PlayBtnBGM(this.sound.can);
           this.miss++;
       }
-      if(this.lang_length -1 < i ) {
-        $(".word").html("");
+      if(this.lang_length -1 < this.langCount ) {
+        this.word.html("");
         this.num += 1;
-        i = 0;
+        this.langCount = 0;
         this.Timearr += this.overTime;
-        this.overTime = 5;
+        this.overTime = this.sec;
         this.action();
       }
-    })
+  }
+
+  char(ca){
+    return this.target_lang[ca].toUpperCase().charCodeAt(0);
+  }
+
+  startAction(e,btn){
+      e.stopPropagation();
+      this.score = Date.now();
+      this.PlayBtnBGM(this.MP().start);
+      btn.addClass("start");
+      setTimeout(() => {
+      this.overlay.addClass("fade");
+      this.Time("timerTxt",2);
+      },3000);
   }
 
   MP(){
     var MP4 = {
-      BGM: new Audio("./audio/test.mp3"),
+      BGM: new Audio("./audio/BGM.mp3"),
       btn: "./audio/btn.mp3",
-      can: "./audio/can.mp3"
+      can: "./audio/can.mp3",
+      start: "./audio/start.mp3"
     };
     return MP4;
   }
@@ -149,9 +159,12 @@ class typing {
   }
 
   Time(el,dobble){
-    var timer,s,update,overTime;
+    var timer,
+        s,
+        update,
+        overTime;
 
-    this.overTime = 5;
+    this.overTime = this.sec;
     s = Date.now();
     this.Timertxt = document.getElementById(el);
     this.update = (dobble) => {
@@ -172,56 +185,106 @@ class typing {
   }
 
   GemeOverTime(){
-      $("#overlay .btn").text("タイムオーバー");
+      this.start_btn.text("タイムオーバー");
       this.removeGame();
-      $(".restart").show();
-      $("#overlay").removeClass("fade");
+      this.restart.show();
+      $(".overtime").text("タイムオーバー").show();
+      this.overlay.find("p").removeClass("start");
+      this.overlay.removeClass("fade");
   }
 
   removeGame(){
     this.sound.BGM.pause();
     this.sound.BGM.currentTime = 0;
-    $(window).off("keydown");
+    this.win.off("keydown");
   }
 
   GameClear(){
       this.removeGame();
       clearInterval(this.Gametimer);
-      $("#overlay .btn").text("ステージクリア！");
-      $(".time").addClass("visible");
-      $("#overlay").removeClass("fade");
+      this.word_label.html("");
+      this.overlay.find("p").removeClass("start");
+      this.start_btn.text("ステージクリア！");
+      this.timeTx.addClass("visible");
+      this.overlay.removeClass("fade");
       var EvaluationTxt = this.Evaluation(this.Timearr);
-      $(".restart").text(EvaluationTxt　+　"<br>ミス"　+　this.miss + "回");
-      $(".restart").show();
+      this.evaluation.text(EvaluationTxt);
+      this.missTxt.text("タイプミス: "　+　this.miss + " 回");
+      this.timeScore.text("クリアータイム: "　+　this.timeCalculation(1) + " 秒");
+      this.restart.show();
+      this.missTxt.show();
   }
 
+  timeCalculation(num){
+    return ((Date.now() - this.score)/1000).toFixed(num);
+  }
 
   Evaluation(val){
-    var _val = Math.floor(val/this.label_.length)
-    var EvaluationTxt;
-  switch(_val){
-    case 5:
-      EvaluationTxt = "あなたはとても優秀ですね"
-      break;
-    case 4:
-      EvaluationTxt = "優秀ですね"
-      break;
-    case 3:
-      EvaluationTxt = "普通ですね"
-      break;
-    case 2:
-      EvaluationTxt = "遅い優秀ですね"
-      break;
-    default:
-      EvaluationTxt = "ダメダメですね"
-      break;
+    var _val = this.EvaluationNum(val,this.label_.length),
+        EvaluationTxt;
+    switch(_val){
+      case 5:
+        EvaluationTxt = "あなたはとても優秀ですね"
+        break;
+      case 4:
+        EvaluationTxt = "優秀ですね"
+        break;
+      case 3:
+        EvaluationTxt = "普通ですね"
+        break;
+      case 2:
+        EvaluationTxt = "遅い優秀ですね"
+        break;
+      default:
+        EvaluationTxt = "ダメダメですね"
+        break;
+    }
+    return EvaluationTxt;
+  }
+
+  EvaluationNum(val,len){
+    return Math.floor(val/len);
   }
 
 
-  return EvaluationTxt;
+  getWord(pas){
+    var WORD = [];
+    var KEYS = [];
+    $.ajax({
+      url: pas,
+      dataType: "json",
+      cache: false,
+      success: (data, textStatus) => {
+      this.json = data.item;
+      this.jsonKeys = Object.keys(data.item);
+      this.set(this.json,this.jsonKeys);
+      },
+      error: function(xhr, textStatus, errorThrown){
+        alert("読み込みに失敗しました");
+      }
+    });
+  }
+
+  UA(ua){
+    var isUA = (function(){
+      var ua = navigator.userAgent.toLowerCase(),
+          indexOfKey = function(key){ return (ua.indexOf(key) != -1)? true: false;};
+      var o = {};
+      o.ie      = function(){ return indexOfKey("msie 8"); }
+      o.fx      = function(){ return indexOfKey("firefox"); }
+      o.chrome  = function(){ return indexOfKey("chrome"); }
+      o.opera   = function(){ return indexOfKey("opera"); }
+      o.android = function(){ return indexOfKey("android"); }
+      o.ipad    = function(){ return indexOfKey("ipad"); }
+      o.ipod    = function(){ return indexOfKey("ipod"); }
+      o.iphone  = function(){ return indexOfKey("iphone"); }
+      return o;
+    })();
+
+  return isUA[ua]();
 
   }
 
 }
+new typing().getWord("json/ikimono.json");
 
-new typing().set();
